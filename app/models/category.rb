a993d4f33d72445350689c09ece2599869f125cf
joaofelipe <joaofelipe1294@gmail.com>
2020-07-15 19:58:55 -0300

    Added recently heard #create route.

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,12 +4,10 @@ class Category < ApplicationRecord
   has_one_attached :image
 
   def artists
-    # Artist.joins(:albuns).where("albuns.id IN(?)", self.albuns.ids).distinct
     Artist.joins(:albums).where(albums: { id: self.albums.ids }).distinct
   end
 
   def songs
-    # Song.joins(:album).where("albums.id IN(?)", self.albums.ids).distinct
     Song.joins(:album).where(songs: { id: self.albums.ids }).distinct
   end
 
