691293aa65c337f424241be1ccaccee203e760b9
joaofelipe <joaofelipe1294@gmail.com>
2020-07-15 12:49:16 -0300

    Added search by ategory.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,16 @@
 class Category < ApplicationRecord
-  has_many :albuns
+  has_many :albums
   validates_presence_of :name
   has_one_attached :image
+
+  def artists
+    # Artist.joins(:albuns).where("albuns.id IN(?)", self.albuns.ids).distinct
+    Artist.joins(:albums).where(albums: { id: self.albums.ids }).distinct
+  end
+
+  def songs
+    # Song.joins(:album).where("albums.id IN(?)", self.albums.ids).distinct
+    Song.joins(:album).where(songs: { id: self.albums.ids }).distinct
+  end
+
 end
