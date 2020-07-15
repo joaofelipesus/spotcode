c2198f4f814b0564eb760acdb5d502dc2d4b3ffd
joaofelipe <joaofelipe1294@gmail.com>
2020-07-14 22:51:21 -0300

    Added dashboard api routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :dashboard, only: :index
+    end
+  end
 end
