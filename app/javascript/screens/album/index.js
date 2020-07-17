41f97d81c468a3e265ba4cf1c602925d513c2172
joaofelipe <joaofelipe1294@gmail.com>
2020-07-17 15:27:13 -0300

    Added albums component.

--- app/javascript/screens/album/index.js
+++ app/javascript/screens/album/index.js
@@ -1,12 +1,16 @@
-import React, { Fragment, useState } from 'react';
-import NavbarFooter from '../../components/common/navbar_footer'
-
+import React, { Fragment } from 'react';
+import NavbarFooter from '../../components/common/navbar_footer';
+import Albums from '../../components/albums';
+import SectionWrapper from '../../components/common/section_wrapper'
+​
 const AlbumScreen = () => {
-  return(
-    <Fragment>
-      <div>Album Screen</div>
-      <NavbarFooter />
-    </Fragment>
-  );
+ return(
+   <Fragment>
+     <SectionWrapper>
+       <Albums/>
+       <NavbarFooter/>
+     </SectionWrapper>
+   </Fragment>
+);
 }
 export default AlbumScreen;
